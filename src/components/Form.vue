c3ae819fa68ce509ec669afccf9faf6af640848d
Ben Bowes <bowesemails@gmail.com>
2019-04-16 14:42:59 +0800

    Add Vuex to manage state

---
+++ src/components/Form.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <label for="flavor">Favorite ice cream flavor?</label>
+    <input @input="changed" name="flavor">
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        changed: function(event) {
+            this.$store.commit('updateFlavor', event.target.value)
+        }
+    }
+}
+</script>
